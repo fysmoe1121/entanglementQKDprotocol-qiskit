69ec386ab356dc038ff9b1f564830d2ff8d9da09
leonma69 <49735696+leonma69@users.noreply.github.com>
2020-12-30 20:03:51 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,61 +1,116 @@
 from qiskit import IBMQ, QuantumCircuit, execute, Aer
 from qiskit.visualization import plot_histogram, plot_bloch_multivector
 from numpy.random import randint
+import random
 import numpy as np
 import matplotlib
-%matplotlib inline
 from math import pi
 
-def create_message():
-    message = []
-    for i in range(0,n):
-        qc = QuantumCircuit(2,2)
+np.random.seed(seed=10)
+n = 100
+alice_bases = randint(3, size=n)
+print(alice_bases)
+
+np.random.seed(seed=11)
+n = 100
+bob_bases = randint(3, size=n)
+print(bob_bases)
+
+def create_eprPairs():
+    pairs = []
+    for i in range(n):
+        qc = QuantumCircuit(2,3)
         qc.x(0)
         qc.x(1)
         qc.h(0)
         qc.cx(0,1)
         qc.barrier()
-        message.append(qc)
-    return(message)
-    
-np.random.seed(seed=0)
-n = 20
-alice_bases = randint(3, size=n)
-print(alice_bases)
+        pairs.append(qc)
+    return(pairs)
 
-np.random.seed(seed=1)
-n = 20
-bob_bases = randint(3, size=n)
-print(bob_bases)
-
-def measure_message(message, alice, bob):
+def measure_pairs(pairs, alice, bob):
     backend = Aer.get_backend('qasm_simulator')
-    alice_measurements = []
-    bob_measurements = []
+    measurements = []
     for q in range(n):
         if alice[q] == 0:
             pass
         if alice[q] == 1:
-            message[q].rz(pi/2, 0)
+            pairs[q].rz(pi/2, 0)
         if alice[q] == 2:
-            message[q].rz(pi/4, 0)
-        message[q].h(0)
-        message[q].measure(0,0)
-        message[q].barrier()
+            pairs[q].rz(pi/4, 0)
+        pairs[q].h(0)
+        pairs[q].measure(0,0)
+        pairs[q].barrier()
         if bob[q] == 0:
             pass
         if bob[q] == 1:
             message[q].rz(-pi/4,1)
         if bob[q] == 2:
-            message[q].rz(pi/4, 1)
-        message[q].h(1)
-        message[q].measure(1,1)
-        result = execute(message[q], backend, shots=1, memory=True).result()
-        alice_measured_bit = (int(result.get_memory()[0]))//10
-        bob_measured_bit = (int(result.get_memory()[0]))%10
-        alice_measurements.append(alice_measured_bit)
-        bob_measurements.append(bob_measured_bit)
-    return(alice_measurements, bob_measurements)
+            message[q].rz(pi/4, 1)        
+        pairs[q].h(1)
+        pairs[q].measure(1,1)
+        result = execute(pairs[q], backend, shots=1, memory=True).result()
+        one_measurement = []
+        alice_measured_bit = (int(result.get_memory()[0][0]))
+        bob_measured_bit = (int(result.get_memory()[0][1]))
+        one_measurement.append(alice_measured_bit)
+        one_measurement.append(bob_measured_bit)
+        measurements.append(one_measurement)
+    return(measurements)
+
+def create_sifted_key(measurement, alice, bob):
+    sifted_key = []
+    for i in range(n):
+        if (alice[i] == bob[i] == 0) or (alice[i] == bob[i] == 2):
+            sifted_bit = []
+            sifted_bit.append(measurement[i][0])
+            sifted_bit.append(measurement[i][1])
+            sifted_key.append(sifted_bit)
+    return(sifted_key, "length of sifted key is " + str(len(sifted_key)))
+
+def expected_value(measurement, alice, bob, alice_setting, bob_setting):
+    pp = 0
+    mm = 0
+    pm = 0
+    mp = 0
+    for i in range(n):
+        if alice[i] == alice_setting and bob[i] == bob_setting:
+            if measurement[i][0] == measurement[i][1] == 1:
+                pp+=1
+            if measurement[i][0] == measurement[i][1] == 0:
+                mm+=1
+            if measurement[i][0] == 1 and measurement[i][1] == 0:
+                pm+=1
+            if measurement[i][0] == 0 and measurement[i][1] == 1:
+                mp+=1
+    return((pp - mp - pm + mm)/(pp + mp + pm + mm))
+
+def CHSH_calc(measurements, alice, bob):
+    S = 0
+    S += expected_value(measurements,alice,bob,0,2)
+    S += expected_value(measurements,alice,bob,0,1)
+    S += expected_value(measurements,alice,bob,1,2)
+    S -= expected_value(measurements,alice,bob,1,1)
+    return(abs(S))
+
+eve_bits = random.sample(range(n),n/4)
+eve_bits.sort()
+print(eve_bits)
+
+def eve_drop(pairs, eve):
+    for i in eve:
+        pairs[i].measure(0,2)
+        pairs[i].barrier()
+        
+def eve_dectected(s_value):
+    if s_value > 2.8:
+        print("eve went undetetcted")
+    elif 2.5 < s_value <= 2.8:
+        print("eve is maybe evedropping")
+    else:
+        print("eve is probably evedropping")
+        
+
     
 
   
